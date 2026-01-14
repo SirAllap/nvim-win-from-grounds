27251c5b48dcd066a9b4276f92e75be87a8dab01
David Pallares Robaina <david.pallares@smith.ai>
2026-01-14 14:15:59 +0100

    context working

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,9 +1,10 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
     opts = {
       enable = true, -- enabled by default
-      max_lines = 3,
+      max_lines = 10,  -- increased from 3 to show more context
       trim_scope = "outer",
       mode = "cursor",
     },
